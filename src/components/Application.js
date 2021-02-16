9286ab943266bce1f695e6d5435158b02ea8139a
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-16 02:54:29 +0000

    added DayList to main applicaion

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,48 @@
-import React from "react";
-
-import "components/Application.scss";
+import React from 'react';
+import 'components/Application.scss';
+import DayList from 'components/DayList.js';
+const days = [
+  {
+    id: 1,
+    name: 'Monday',
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: 'Tuesday',
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: 'Wednesday',
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
   return (
-    <main className="layout">
-      <section className="sidebar">
+    <main className='layout'>
+      <section className='sidebar'>
         <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
+          className='sidebar--centered'
+          src='images/logo.png'
+          alt='Interview Scheduler'
         />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <hr className='sidebar__separator sidebar--centered' />
+        <nav className='sidebar__menu'>
+          <DayList
+            days={days}
+            day={'Monday'}
+            setDay={(day) => console.log(day)}
+          />
+        </nav>
         <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
+          className='sidebar__lhl sidebar--centered'
+          src='images/lhl.png'
+          alt='Lighthouse Labs'
         />
       </section>
-      <section className="schedule">
+      <section className='schedule'>
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
