dd404126a26e8d2d19d91c12a4eb1a13a2dc2af1
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-17 04:50:04 +0000

    added Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Header from './Header';
-import Confirm from './Confirm';
 import Empty from './Empty';
-import Error from './Error';
-import Form from './Form';
-import index from './index';
 import Show from './Show';
-import Status from './Status';
 import './styles.scss';
 
-const Appointment = () => {
-  return <div></div>;
+const Appointment = ({ time, interview }) => {
+  return (
+    <div className='appointment'>
+      <Header time={time} />
+      {interview ? (
+        <Show student={interview.student} interviewer={interview.interviewer} />
+      ) : (
+        <Empty />
+      )}
+    </div>
+  );
 };
 
 export default Appointment;
