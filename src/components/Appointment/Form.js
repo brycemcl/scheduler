434d9f6a38c9d3ab3524cb98e2de81ae25edc882
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-17 04:09:12 +0000

    added form subcomponent of the Appointment component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,7 +1,47 @@
-import React from 'react';
-
-const Form = () => {
-  return <div></div>;
+import React, { useState } from 'react';
+import InterviewerList from 'components/InterviewerList';
+import Button from 'components/Button';
+const Form = ({
+  name: propsName = '',
+  interviewers,
+  interviewer: propsInterviewer = '',
+  onSave,
+  onCancel,
+}) => {
+  const [name, setName] = useState(propsName);
+  const [interviewer, setInterviewer] = useState(propsInterviewer);
+  return (
+    <main className='appointment__card appointment__card--create'>
+      <section className='appointment__card-left'>
+        <form autoComplete='off'>
+          <input
+            className='appointment__create-input text--semi-bold'
+            name='name'
+            type='text'
+            placeholder='Enter Student Name'
+            /*
+          This must be a controlled component
+        */
+          />
+        </form>
+        <InterviewerList
+          interviewers={interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className='appointment__card-right'>
+        <section className='appointment__actions'>
+          <Button danger onClick={onCancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={onSave}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
 };
 
 export default Form;
--- stories/index.js
+++ stories/index.js
@@ -167,7 +167,42 @@ storiesOf('Appointment', module)
   .add('Appointment with Time', () => <Appointment time='12pm' />)
   .add('Header', () => <Header time='12pm' />)
   .add('Empty', () => <Empty onAdd={action('onAdd')} />)
-  .add('Show', () => <Show student="Lydia Miller-Jones" interviewer={ interviewer} onEdit={action('onEdit')} onDelete={action('onDelete')} />)
-  .add('Confirm', () => <Confirm message="Delete the appointment?" onConfirm={action('onConfirm')} onCancel={action('onCancel')} />)
-  .add('Status', () => <Status message="Deleting" />)
-  .add('Error', () => <Error message="Could not delete appointment." onClose={action('onClose')} />)
+  .add('Show', () => (
+    <Show
+      student='Lydia Miller-Jones'
+      interviewer={interviewer}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ))
+  .add('Confirm', () => (
+    <Confirm
+      message='Delete the appointment?'
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add('Status', () => <Status message='Deleting' />)
+  .add('Error', () => (
+    <Error
+      message='Could not delete appointment.'
+      onClose={action('onClose')}
+    />
+  ))
+  .add('Edit Form', () => (
+    <Form
+      name='Sally Smith'
+      interviewers={interviewers}
+      interviewer={1}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add('Create Form', () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ));
+  
