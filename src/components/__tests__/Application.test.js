36dda8fc4f6f4a77f5a878caf18bfdfc48e41396
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-23 05:45:07 +0000

    improved test for Application component

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import { render, cleanup, waitForElement } from '@testing-library/react';
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+} from '@testing-library/react';
 
 import Application from 'components/Application';
 
@@ -9,6 +14,9 @@ describe('Application', () => {
   it('defaults to Monday and changes the schedule when a new day is selected', () => {
     const { getByText } = render(<Application />);
 
-    return waitForElement(() => getByText('Monday'));
+    return waitForElement(() => getByText('Monday')).then(() => {
+      fireEvent.click(getByText('Tuesday'));
+      expect(getByText('Lydia Miller-Jones')).toBeInTheDocument();
+    });
   });
 });
